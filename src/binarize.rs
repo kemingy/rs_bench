c0fefd81ca78bea25b8c8f0b9c055b2b24ace262
Keming <kemingy94@gmail.com>
2024-08-22 12:05:28 +0800

    go branchless in binarize avx2
    Signed-off-by: Keming <kemingy94@gmail.com>

--- src/binarize.rs
+++ src/binarize.rs
@@ -26,8 +26,8 @@ pub unsafe fn vector_binarize_avx2(vec: &[u8]) -> Vec<u64> {
         v = _mm256_slli_epi32(v, 4);
         for j in 0..THETA_LOG_DIM as usize {
             let mask = (_mm256_movemask_epi8(v) as u32).reverse_bits() as u64;
-            // let shift = ((i >> 5) & 1) << 5;
-            let shift = if (i / 32) % 2 == 0 { 32 } else { 0 };
+            // let shift = if (i / 32) % 2 == 0 { 32 } else { 0 };
+            let shift = (((i >> 5) ^ 1) & 1) << 5;
             binary[(3 - j) * (length >> 6) + (i >> 6)] |= mask << shift;
             v = _mm256_slli_epi32(v, 1);
         }
@@ -41,7 +41,7 @@ fn test_binarize() {
     use rand::{thread_rng, Rng};
 
     let mut rng = thread_rng();
-    for size in [960usize].into_iter() {
+    for size in [128].into_iter() {
         let vec: Vec<u8> = (0..size).map(|_| rng.gen::<u8>()).collect();
         assert_eq!(query_vector_binarize(&vec), unsafe {
             vector_binarize_avx2(&vec)
